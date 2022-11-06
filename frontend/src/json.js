9b3534549ac8d8363d63e062ddafd6e5c0afb147
Cainan Conway <conway.cainan@ufl.edu>
2022-11-06 16:32:57 -0500

    basic liveserver comparison page

---
+++ frontend/src/json.js
@@ -0,0 +1,36 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://t3.ftcdn.net/jpg/04/34/72/82/240_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg"
+}, {
+    "id": 1,
+    "title": "McRib",
+    "price": "4.59",
+    "description": "The McRib is a barbecue-flavored pork sandwich periodically sold by the international fast food restaurant chain McDonald's. It was first introduced to the McDonald's menu in 1981, following test marketing the year before.",
+    "brand": "McDonalds",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/McD-McRib.jpg/1200px-McD-McRib.jpg"
+}, {
+    "id": 2,
+    "title": "Cinnabon Delights, 2 pack",
+    "price": "1.89",
+    "description": "Cinnabon Delights are a dessert and a breakfast item served at Taco Bell. They are round pieces of bread covered with sugar and have an icing center. They come in a 2-pack, a 4-pack and a 12-pack.",
+    "brand": "TacoBell",
+    "image": "https://www.tacobell.com/images/30046_cinnabon_delights_2_pack_269x269.jpg"
+}, {
+    "id": 3,
+    "title": "Baja Blast",
+    "price": "2.29",
+    "description": "Mountain Dew Baja Blast is a tropical lime-flavored soft drink under the Mountain Dew brand, exclusively distributed at Taco Bell restaurants. Since the original flavor's launch in 2004, the Baja Blast brand has since expanded to include new flavors, alcoholic beverages, and energy drinks, as well as merchandise, such as flavored lip balm, room sprays, and Halloween costumes.",
+    "brand": "TacoBell",
+    "image": "https://www.tacobell.com/images/1027_mountain_dew_baja_blast_269x269.jpg"
+}, {
+    "id": 4,
+    "title": "McChicken + McDouble",
+    "price": "4.78",
+    "description": "The McGangBang, also called the Bababooey Burger is a very popular, if not the most popular, Secret Menu item McDonald's has to offer. The absurd and grotesque title gives a look into the nature of the burger. The McGangbang originally started by putting a McChicken (Or Junior Chicken in Canada) in between a Double Cheeseburger or McDouble. It was a way to get more \"bang\" for your buck when ordering from the Value Menu. The modern variation of the McGangBang is made out of a McSausage (or McShmeaty), but instead of placing it between a McDouble or Double Cheese, it's placed between a Big Mac and the Big Mac's middle bun is removed to create the McGangBang.",
+    "brand": "McDonalds",
+    "image": "http://hackthemenu.com/wp-content/uploads/2015/11/mcdonalds-mcgangbang-1032px.jpg"
+}]
