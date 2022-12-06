88ae3fe774c3b4a9767b7db87b6046b290956212
NickYoungren <95592811+NickYoungren@users.noreply.github.com>
2022-12-06 11:39:31 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,26 @@ import mongodb from "mongodb"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import ProductsDAO from "./dao/productsDAO.js"
-import AuthDAO from "./dao/authDAO.js"
+import AuthDAO from "./dao/authDAO.js"            //import functions
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8080
 
-mongoose 
+mongoose                                        //Mongoose client is used to connect to user database to create user schemas
  .connect(process.env.PRODUCTS_DB_URI, {
-        useNewUrlParser: true,
+        useNewUrlParser: true,                 //useNewUrlParser will be deprecated in future versions
         useUnifiedTopology: true,
         })   
  .then(() => console.log("Database connected!"))
  .catch(err => console.log(err));
 
 
-MongoClient.connect(
+MongoClient.connect(                           //MongoClient handles the product information
     process.env.PRODUCTS_DB_URI,
     {
         maxPoolSize: 50,
-        socketTimeoutMS: 2500,
+        socketTimeoutMS: 2500,    
         useNewUrlParser: true
     }
 )
