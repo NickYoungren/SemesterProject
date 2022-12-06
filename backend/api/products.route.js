fe565cb5559bb325441b0dd774eaf27613a304de
Jake Shirey <jake.shirey@gmail.com>
2022-12-05 23:20:40 -0500

    user auth

--- backend/api/products.route.js
+++ backend/api/products.route.js
@@ -1,5 +1,7 @@
 import express from "express"
 import ProductsCtrl from "./products.controller.js"
+import AuthDAO from "../dao/authDAO.js"
+
 
 const router = express.Router()
 
@@ -7,5 +9,7 @@ const router = express.Router()
 router.route("/").get(ProductsCtrl.apiGetProducts) //done converting
 router.route("/id/:id").get(ProductsCtrl.apiGetRestaurantById)
 router.route("/brands").get(ProductsCtrl.apiGetProductBrands)
+router.route("/signup").post(AuthDAO.signup)
+router.route("/signin").post(AuthDAO.signin)
 
 export default router
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 import ProductsDAO from "./dao/productsDAO.js"
-import ReviewDAO from "./dao/reviewDAO.js"
+import AuthDAO from "./dao/authDAO.js"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8080
+
+mongoose 
+ .connect(process.env.PRODUCTS_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        })   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
+
 
 MongoClient.connect(
     process.env.PRODUCTS_DB_URI,
@@ -22,7 +32,7 @@ MongoClient.connect(
 })
 .then(async client => {
     await ProductsDAO.injectDB(client)
-    await ReviewDAO.injectDB(client)
+    await AuthDAO.injectDB(client)
     app.listen(port, () => {
         console.log(`listening on port ${port}`)
     })
