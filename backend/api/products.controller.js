cb3f51998f4eada9a0233dc2f91fc989df886ed9
NickYoungren <95592811+NickYoungren@users.noreply.github.com>
2022-12-06 11:55:48 -0500

    Update products.controller.js

--- backend/api/products.controller.js
+++ backend/api/products.controller.js
@@ -1,8 +1,10 @@
 import ProductsDAO from "../dao/productsDAO.js"
 
 export default class ProductsController {
+    
+    //obtains a page worth of products from a colleciton in the database
     static async apiGetProducts(req, res, next){
-        const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage, 10) : 10
+        const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage, 10) : 10        //default product page will include 10 items
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
         let filters = {}
